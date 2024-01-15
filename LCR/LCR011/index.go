db485e9fb113e87480ad24fac5c2219a01e8fb0f
孟翔 <xmeng@elsvision.cn>
2024-01-15 17:03:58 +0800

    style: 修改语法问题

--- LCR/LCR011/index.go
+++ LCR/LCR011/index.go
@@ -2,7 +2,7 @@ package LCR011
 
 // 给定一个二进制数组 nums , 找到含有相同数量的 0 和 1 的最长连续子数组，并返回该子数组的长度。
 func findMaxLength(nums []int) int {
-	if nums == nil || len(nums) == 0 {
+	if len(nums) <= 0 {
 		return 0
 	}
 
