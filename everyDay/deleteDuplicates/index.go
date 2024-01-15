1ad877cba7718797562b23d9e4c43e67f4e89252
孟翔 <xmeng@elsvision.cn>
2024-01-15 16:16:11 +0800

    feat: 完成LCR010

--- everyDay/deleteDuplicates/index.go
+++ everyDay/deleteDuplicates/index.go
@@ -8,17 +8,13 @@ import (
 func deleteDuplicates(head *ListNode) *ListNode {
 	stack := list.New()
 
-	result = nil
+	result := &ListNode{
+		Val:  head.Val,
+		Next: nil,
+	}
 
 	for head != nil {
-		if stack.Len() == 0 {
-			stack.PushBack(head.Val)
-			// 创建一个新的节点
-			result = &ListNode{
-				Val:  head.Val,
-				Next: nil,
-			}
-		} else if head.Val != stack.Back().Value.(int) {
+		if head.Val != stack.Back().Value.(int) {
 			stack.PushBack(head.Val)
 			result.Next = &ListNode{
 				Val:  head.Val,
