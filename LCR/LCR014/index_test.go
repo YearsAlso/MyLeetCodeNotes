88b32227751d239c403787c1584b5fd4b0c9d327
YearsAlso <1334713380@qq.com>
2024-01-17 21:15:34 +0800

    feat: 完成LCR014

--- LCR/LCR014/index_test.go
+++ LCR/LCR014/index_test.go
@@ -20,6 +20,20 @@ func Test_checkInclusion(t *testing.T) {
 				s2: "eidboaoo",
 			},
 			want: false,
+		}, {
+			name: "LCR014 case2",
+			args: args{
+				s1: "abc",
+				s2: "cccccbabbbaaaa",
+			},
+			want: true,
+		}, {
+			name: "LCR014 case3",
+			args: args{
+				s1: "adc",
+				s2: "dcda",
+			},
+			want: true,
 		},
 	}
 	for _, tt := range tests {
---
+++ LCR/LCR015/index.go
@@ -0,0 +1,5 @@
+package LCR015
+
+func findAnagrams(s string, p string) []int {
+	return nil
+}
