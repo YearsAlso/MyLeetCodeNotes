be01c241ba1931cfb8c786fd2da4cb30aa6bacc2
YearsAlso <1334713380@qq.com>
2024-01-17 21:36:03 +0800

    feat: 完成LCR015

--- LCR/LCR015/index_test.go
+++ LCR/LCR015/index_test.go
@@ -16,6 +16,14 @@ func Test_findAnagrams(t *testing.T) {
 		want []int
 	}{
 		// TODO: Add test cases.
+		{
+			name: "LCR015 case1",
+			args: args{
+				s: "cbaebabacd",
+				p: "abc",
+			},
+			want: []int{0, 6},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
