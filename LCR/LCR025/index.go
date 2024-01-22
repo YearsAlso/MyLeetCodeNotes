29e6517e4fa5c5440246d0e07578f7b125f36d22
孟翔 <xmeng@elsvision.cn>
2024-01-22 18:37:25 +0800

    feat: LCR025 没有开发完成

--- LCR/LCR025/index.go
+++ LCR/LCR025/index.go
@@ -19,7 +19,49 @@ type ListNode = definition.ListNode
 可以假设除了数字 0 之外，这两个数字都不会以零开头。
 */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	result := (*ListNode)(nil)
+	num1List := convertToNumber(l1)
+	num2List := convertToNumber(l2)
+
+	// 找到最长的那个
+
+	var result *ListNode = nil
+	result = nil
+
+	l := max(len(num1List), len(num2List))
+
+	add := 0
+
+	for i = l - 1; i >= 0; i-- {
+		num := 0
+		if i < len(num1List) && i < len(num2List) {
+			num = num1List[i] + num2List[i] + add
+			add = num / 10
+		} else if i < len(num1List) {
+			num = num1List[i] + add
+			add = num / 10
+		} else if i < len(num2List) {
+			num = num2List[i] + add
+			add = num / 10
+		}
+		result = &ListNode{
+			Val:  num,
+			Next: result,
+		}
+	}
+
+	return result
+}
+
+func convertToNumber(l *ListNode) []int {
+	result := make([]int, 0)
+
+	for {
+		if l == nil {
+			break
+		}
+		result = append(result, l.Val)
+		l = l.Next
+	}
 
 	return result
 }
