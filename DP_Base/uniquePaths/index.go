6966c814296067b1e403a132530a4d1905c12048
YearsAlso <1334713380@qq.com>
2024-01-30 22:40:56 +0800

    feat: 完成dp问题

--- DP_Base/uniquePaths/index.go
+++ DP_Base/uniquePaths/index.go
@@ -8,7 +8,7 @@ package uniquePaths
 问总共有多少条不同的路径？
 */
 
-func uniquePaths(m int, n int) int {
+func uniquePaths1(m int, n int) int {
 	if m <= 0 || n <= 0 {
 		return 0
 	}
@@ -17,5 +17,26 @@ func uniquePaths(m int, n int) int {
 		return 1
 	}
 
-	return uniquePaths(m-1, n) + uniquePaths(m, n-1)
+	return uniquePaths1(m-1, n) + uniquePaths1(m, n-1)
+}
+
+// 这个方法会占用较多的内存，可以使用一行数组进行保存
+// 递归转遍历，可以将递归的内容写出公式，然后进行推演，将所有的条件放到数组中求解，就能得到最终的结果
+func uniquePaths(m int, n int) int {
+	dp := make([][]int, m)
+	for i := range dp {
+		dp[i] = make([]int, n)
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if i == 0 || j == 0 {
+				dp[i][j] = 1
+			} else {
+				dp[i][j] = dp[i-1][j] + dp[i][j-1]
+			}
+		}
+	}
+
+	return dp[m-1][n-1]
 }
